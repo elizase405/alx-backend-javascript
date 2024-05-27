eab9b79b81ae8c08a23ce351da792fb7081af9c7
Elizabeth <elizase405@gmail.com>
2024-05-27 20:54:31 +0000

    feat: task 1

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+// Use arrow function
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
