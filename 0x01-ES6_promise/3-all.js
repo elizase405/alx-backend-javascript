3bb56dc61d7593666e0692cbcfe6993552ee45f5
Elizabeth <elizase405@gmail.com>
2024-11-13 16:27:20 +0000

    fix: change console.error to console.log

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,5 +7,5 @@ export default function handleProfileSignup() {
   const cu = createUser();
   return Promise.all([up, cu])
     .then(([res1, res2]) => console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`))
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
