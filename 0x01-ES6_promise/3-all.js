92d9d394755537ffbea72af6d23b55f36dfb690d
Elizabeth <elizase405@gmail.com>
2024-11-13 16:06:10 +0000

    feat: task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+/* eslin-tdisable no-unused-vars */
+// Handle multiple successful promises
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const up = uploadPhoto();
+  const cu = createUser();
+  return Promise.all([up, cu])
+    .then(([res1, res2]) => console.log(res1.body, res2.firstName, res2.lastName))
+    .catch(() => console.error('Signup system offline'));
+}
