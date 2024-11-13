ce00b300e545ef2d094effef0b088eaee5721c91
Elizabeth <elizase405@gmail.com>
2024-11-13 17:19:37 +0000

    misc: fix lint err

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const su = signUpUser(firstName, lastName)
-  const up = uploadPhoto(fileName)
+  const su = signUpUser(firstName, lastName);
+  const up = uploadPhoto(fileName);
 
   return Promise.allSettled([su, up])
     .then((result) => console.log(result));
