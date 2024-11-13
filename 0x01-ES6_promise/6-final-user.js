4ceae4fde67a7b6154fbca7e29999a6c92e3234a
Elizabeth <elizase405@gmail.com>
2024-11-13 17:44:27 +0000

    misc: fix lint errs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,8 +6,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const up = uploadPhoto(fileName);
 
   return Promise.allSettled([su, up])
-    .then((results) => results.map(result => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value: `Error: ${result.reason.message}`,
+      value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
     })));
 }
