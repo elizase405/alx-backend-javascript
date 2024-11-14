103bdff0e51db7f4d5d732733164d0b6b6d594a1
Elizabeth <elizase405@gmail.com>
2024-11-14 17:20:00 +0000

    misc: lint error

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload])
-console.log(loadBalancer(promiseUK, promiseFR))
     .then((res1) => res1)
     .catch((err) => console.log(err));
 }
