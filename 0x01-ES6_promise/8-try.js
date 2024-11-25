fca92dd4f53dbb128bb0535505c92c292407534a
Elizabeth <elizase405@gmail.com>
2024-11-25 10:33:22 +0000

    feat: task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,7 @@
 export default function divideFunction(numerator, denominator) {
+  let res = numerator / denominator;
   if (denominator === 0) {
     return new Error('cannot divide by 0');
   }
-  return numerator / denominator;
+  return res;
 }
