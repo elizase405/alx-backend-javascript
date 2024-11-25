e1534bb7667ecae6286d2d5cf4b350f8459a4799
Elizabeth <elizase405@gmail.com>
2024-11-25 14:27:47 +0000

    fix: handle errors properly

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,7 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   let data1 = null;
   let data2 = null;
-  data1 = await uploadPhoto();
-  data2 = await createUser();
-  return {photo: data1, user: data2};
+  try {
+    data1 = await uploadPhoto();
+    data2 = await createUser();
+    return { photo: data1, user: data2 };
+  } catch (err) {
+    return { photo: data1, user: data2 };
+  }
 }
