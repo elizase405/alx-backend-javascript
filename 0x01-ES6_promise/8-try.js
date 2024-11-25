0fb9bdda8241ac8e60e401537689b48e207938b6
Elizabeth <elizase405@gmail.com>
2024-11-25 10:43:39 +0000

    fix: add throw

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   let res = numerator / denominator;
   if (denominator === 0) {
-    return new Error('cannot divide by 0');
+   throw new Error('cannot divide by 0');
   }
   return res;
 }
