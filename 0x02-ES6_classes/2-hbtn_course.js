91f491fc0871c5deafe1ed5a480331edd584eae7
Elizabeth <elizase405@gmail.com>
2024-11-27 15:31:23 +0000

    feat: task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    if (Array.isArray(students) && students.every((val) => typeof val === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+      return this._name;
+    }
+    throw new TypeError('Name must be a string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+      return this._length;
+    }
+    throw new TypeError('Length must be a number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every((val) => typeof val === 'string')) {
+      this.students = newStudents;
+      return this._students;
+    }
+    throw new TypeError('Students must be an array of strings');
+  }
+}
