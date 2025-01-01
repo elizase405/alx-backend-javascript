ed3551dc1c2367beacdc74183155da5a10a45d83
Elizabeth <elizase405@gmail.com>
2025-01-01 20:21:44 +0000

    feat: task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a Number.');
+    } else if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a Number.');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
